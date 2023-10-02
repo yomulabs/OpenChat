e4edcd29ad63401947b379abfc8438f4f6e909b4
Reginald Tan <redge.tan@gmail.com>
2023-10-02 15:51:01 -0400

    disable multithread

--- dj_backend_server/api/data_sources/pdf_handler.py
+++ dj_backend_server/api/data_sources/pdf_handler.py
@@ -12,19 +12,27 @@ from api.interfaces import StoreOptions
 @csrf_exempt
 def pdf_handler(shared_folder: str, namespace: str):
     try:
+        print("PDF_HANDLER STARTING...")
+        print(shared_folder)
+        print(namespace)
         directory_path = os.path.join("website_data_sources", shared_folder)
 
-        directory_loader = DirectoryLoader(path=directory_path, glob="**/*.pdf", loader_cls=PyPDFium2Loader, use_multithreading=True)
+        print("directory loader")
+        directory_loader = DirectoryLoader(path=directory_path, glob="**/*.pdf", loader_cls=PyPDFium2Loader, use_multithreading=False)
 
         raw_docs = directory_loader.load_and_split()
 
+        print("text splitter")
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200,length_function=len)
         docs = text_splitter.split_documents(raw_docs)
 
+        print("get embeddings..")
         embeddings = get_embeddings()
+        print("Init vector store..")
 
         init_vector_store(docs, embeddings, StoreOptions(namespace))
         
+        print("Delete folder..")
         delete_folder(folder_path=directory_path)
         print('All is done, folder deleted')
 
